fdf140f313e135a94c7d49b5a34af11e7fe27e8c
wellsonjain <wellsonwei@ecoworkinc.com>
2015-09-02 00:33:41 +0800

    Add devise and cancan to implement authenticate and authorize
    Signed-off-by: wellsonjain <wellsonwei@ecoworkinc.com>

---
+++ engines/core/app/controllers/samurai/admin/users_controller.rb
@@ -0,0 +1,10 @@
+module Samurai
+  module Admin
+    class UsersController < AdminController
+
+      def index
+        @users = Samurai::User.ordered
+      end
+    end
+  end
+end
--- engines/core/lib/samurai/core.rb
+++ engines/core/lib/samurai/core.rb
@@ -1,3 +1,9 @@
+require 'sass-rails'
+require 'bootstrap-sass'
+require 'autoprefixer-rails'
+require 'devise'
+require 'cancan'
+
 module Samurai
   module Core
   end
